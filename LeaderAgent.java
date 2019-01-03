47c96c8699815bc4784dedf2cdf1b79fd14568d6
Pierrick Pamart <pierrick.pamart@telecom-sudparis.eu>
2019-01-03 18:20:53 +0100

    LeaderAgent register to DFS

--- LeaderAgent.java
+++ LeaderAgent.java
@@ -28,11 +28,38 @@ import java.util.ArrayList;
 
 import jade.core.AID;
 
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+
 public class LeaderAgent extends PlayerAgent {
 	
 	
 	protected void setup() {
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.setName(getAID());
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("team-leader");
+		sd.setName("OverJADE-team-leader");
+		dfd.addServices(sd);
+		try {
+			DFService.register(this, dfd);
+		}
+		catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
 		addBehaviour( new LeaderBehaviour(this));
+	}
+
+	protected void takeDown() {
+		//Deregister from the yellow pages
+		try {
+			DFService.deregister(this);
+		}
+		catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
 	}
 	
 }
